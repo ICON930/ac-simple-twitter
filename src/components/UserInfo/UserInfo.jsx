17067dee3235cd92b6617d481b7fb3c8c39e5586
Keely <hellennnnn1@gmail.com>
2023-08-29 11:32:17 +0800

    Add UserPageApi初版

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -11,9 +11,11 @@ import styles from "./UserInfo.module.scss"
 import Banner from "../../assets/icons/default-banner.svg"
 import Avatar from "../../assets/icons/default-avatar.svg"
 import EditModal from "components/Modal/EditModal"
+import Ball from "../../assets/icons/noti-Icon.svg"
 
-export default function UserInfo() {
+export default function UserInfo({ isOtherUser, userData }) {
 
+  console.log('userData',userData)
   const [isOpenModal, setIsOpenModal] = useState(false)
 
   const openModal = () => {
@@ -29,26 +31,31 @@ export default function UserInfo() {
     <div className={styles.userInfoContainer}>
       <img className={styles.banner} src={Banner} alt="default-banner" /> 
       <img className={styles.avatar} src={Avatar} alt="default-avatar" />
+      {isOtherUser && <img className={styles.ballStyle} src={Ball} alt="ball"/>}
+      {isOtherUser && <img className={styles.ballStyleTwo} src={Ball} alt="ball"/>}  
       <div className={styles.buttonContainer}>
         <Button 
-          title="編輯個人資料"
-          size="edit"
+          title={isOtherUser? "正在跟隨" : "編輯個人資料"}
+          size={isOtherUser? "middle" : "edit"}
           onClick={openModal}
           isAction
+          className={isOtherUser && styles.buttonClass }
         />
       </div>
-        <EditModal isOpen={isOpenModal} isClose={closeModal}/>
+        <EditModal isOpen={isOpenModal} isClose={closeModal} userData={userData}/>
       <div>
         <div className={styles.nameContainer}>
-          <p>name</p>
-          <p>@account</p>
+          <p>{userData.name}</p>
+          <p>@{userData.account}</p>
         </div>
         <div className={styles.introContainer}>
-          <p>description</p>
+          <p>{userData.introduction}</p>
         </div>
         <div className={styles.followerContainer}>
-          <span className={styles.number}>count 個</span><span className={styles.label}>跟隨中</span>
-          <span className={styles.number}>count 位</span><span className={styles.label}>跟隨者</span>
+          <span className={styles.number}>{userData.followingsCount} 個</span>
+          <span className={styles.label}>跟隨中</span>
+          <span className={styles.number}>{userData.followingsCount} 位</span>
+          <span className={styles.label}>跟隨者</span>
         </div>
       </div>
     </div>

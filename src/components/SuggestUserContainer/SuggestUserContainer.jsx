d5ce949005ce629111759086b4c4eb08d0373e4a
ICON930 <kdsc91041@gmail.com>
2023-08-30 14:33:56 +0800

    SuggestUserApi

--- src/components/SuggestUserContainer/SuggestUserContainer.jsx
+++ src/components/SuggestUserContainer/SuggestUserContainer.jsx
@@ -1,23 +1,43 @@
+import { useState, useEffect } from 'react';
+
 import SuggestUserItem from "../SuggestUserItem/SuggestUserItem.jsx";
+import { getTopUsers } from '../../api/user.js'
+
 import styles from "./SuggestUserContainer.module.scss";
 
 export default function SuggestUserContainer() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+    const getTopUser = async () => {
+        try {
+            const data = await getTopUsers();
+            if (data) {
+                setUsers(data);
+            }
+        } catch (error) {
+            console.log('獲取推文失敗', error);
+        }
+        }
+        getTopUser();
+    }, []);
+
+    const topUserList = users.map((user) => {
+        return (
+        <SuggestUserItem
+            key={user.id}
+            avatar={user.avatar}
+            name={user.name}
+            account={user.account}
+        />
+        );
+    });
+
     return (
         <div className={styles.container}>
             <h4 className={styles.title}>推薦跟隨</h4>
             <div className={styles.list}>
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
-                <SuggestUserItem />
+                {topUserList}
             </div>
         </div>
     );
--- src/components/SuggestUserItem/SuggestUserItem.jsx
+++ src/components/SuggestUserItem/SuggestUserItem.jsx
@@ -2,15 +2,15 @@ import Button from "../Button/Button.jsx";
 import logo from "../../assets/icons/default-avatar.svg";
 import styles from "./SuggestUserItem.module.scss";
 
-export default function SuggestUserItem() {
+export default function SuggestUserItem({ avatar, name, account }) {
     return (
         <div className={styles.container}>
             <div className={styles.avatarContainer}>
-                <img className={styles.userAvatar} src={logo} alt="avatar" />
+                <img className={styles.userAvatar} src={avatar || logo} alt="avatar" />
             </div>
             <div className={styles.userInfo}>
-                <p className={styles.userInfoName}>Followers</p>
-                <p className={styles.userInfoAccount}>{`Followers`}</p>
+                <p className={styles.userInfoName}>{name}</p>
+                <p className={styles.userInfoAccount}>@{account}</p>
             </div>
             <div className={styles.btnFollow}>
                 <Button title="正在跟隨" size="middle" isAction></Button>

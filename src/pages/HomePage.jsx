8cf4e7316e55734df7638fc4c36b14066d39785b
ICON930 <kdsc91041@gmail.com>
2023-09-01 07:14:58 +0800

    add HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useAuth } from '../contexts/AuthContext';
+
 const HomePage = () => {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
-}
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/main');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate, isAuthenticated]);
+};
 
-export default HomePage
+export default HomePage;
 

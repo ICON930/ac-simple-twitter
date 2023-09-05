b9cec81a2c90f76f7862b3fa24be54ad63c1833a
aray81205 <aray81205@gmail.com>
2023-09-05 13:29:02 +0800

    修復後台跑版

--- src/pages/AdminUserPage/AdminUserPage.jsx
+++ src/pages/AdminUserPage/AdminUserPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useAuth } from "../../contexts/AuthContext.jsx";
@@ -6,66 +6,63 @@ import { useAuth } from "../../contexts/AuthContext.jsx";
 import NavContainer from "../../components/NavContainer/NavContainer.jsx";
 import Header from "../../components/Header/Header.jsx";
 import AdminUserCard from "../../components/AdminUserCard/AdminUserCard.jsx";
-import { adminGetAllUsers } from '../../api/admin.js';
+import { adminGetAllUsers } from "../../api/admin.js";
 
 import styles from "./AdminUserPage.module.scss";
 
-export default function AdminUserPage () {
+export default function AdminUserPage() {
   const [users, setUsers] = useState([]);
   const { adminAuthenticated } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
-  const adminGetAllUser = async () => {
+    const adminGetAllUser = async () => {
       try {
-          const data = await adminGetAllUsers();
-          if (data.status === 'error') {
-              console.log(data.message);
-              return;
-          }
-          if (data) {
-              setUsers(data);
-          }
+        const data = await adminGetAllUsers();
+        if (data.status === "error") {
+          console.log(data.message);
+          return;
+        }
+        if (data) {
+          setUsers(data);
+        }
       } catch (error) {
-          console.log('獲取使用者失敗', error);
+        console.log("獲取使用者失敗", error);
       }
-      }
-      adminGetAllUser();
+    };
+    adminGetAllUser();
   }, []);
 
   const userList = users.map((user) => {
-        return (
-        <AdminUserCard
-            key={user.id}
-            avatar={user.avatar}
-            name={user.name}
-            cover={user.cover}
-            account={user.account}
-            followerAmount={user.followerAmount}
-            followingAmount={user.followingAmount}
-            tweetsAmount={user.tweetsAmount}
-            likedAmount={user.likedAmount}
-            role={user.role}
-        />
-        );
-    });
+    return (
+      <AdminUserCard
+        key={user.id}
+        avatar={user.avatar}
+        name={user.name}
+        cover={user.cover}
+        account={user.account}
+        followerAmount={user.followerAmount}
+        followingAmount={user.followingAmount}
+        tweetsAmount={user.tweetsAmount}
+        likedAmount={user.likedAmount}
+        role={user.role}
+      />
+    );
+  });
 
   useEffect(() => {
-      if (!adminAuthenticated) {
-          navigate('/admin/login');
-      }
+    if (!adminAuthenticated) {
+      navigate("/admin/login");
+    }
   }, [navigate, adminAuthenticated]);
-
 
   return (
     <div className={styles.container}>
-      <NavContainer page="admin"/>
+      <NavContainer page="admin" />
       <div className={styles.cardContainer}>
         <Header title="使用者列表" />
-        <div className={styles.card}>
-          {userList}
-        </div>
+        <div className={styles.card}>{userList}</div>
       </div>
     </div>
-  )
+  );
 }
